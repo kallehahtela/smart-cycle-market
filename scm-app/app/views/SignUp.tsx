a15000930c5f28628b2d71a38bf7b9810e4d6006
Kalle Hahtela <khahtela@gmail.com>
2024-06-26 22:35:22 +0300

    Capturing Input Change, Validating Form & Refractoring Validator.

--- scm-app/app/views/SignUp.tsx
+++ scm-app/app/views/SignUp.tsx
@@ -1,5 +1,5 @@
 import { View, StyleSheet } from 'react-native'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import colors from '@utils/colors';
 import WelcomeHeader from '@ui/WelcomeHeader';
 import FormInput from '@ui/FormInput';
@@ -9,22 +9,63 @@ import FormNavigator from '@ui/FormNavigator';
 import CustomKeyAvoidingView from '@ui/CustomKeyAvoidingView';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { AuthStackParamList } from 'app/navigator/AuthNavigator';
+import * as yup from 'yup';
+import axios from 'axios';
+import { newUserSchema, yupValidate } from '@utils/validator';
+
+
 
 interface Props {}
 
-const SignUp: FC<Props> = (props) => {
+const SignUp: FC<Props> = () => {
+  const [userInfo, setUserInfo] = useState({
+    name: '', 
+    email: '', password: ''
+  });
   const { navigate } = useNavigation<NavigationProp<AuthStackParamList>>(); 
+
+  const handleChange = (name: string) => (text: string) => {
+    setUserInfo({...userInfo, [name]: text});
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const { values, error } = await yupValidate(newUserSchema, userInfo);
+      if (error) {
+        console.log(error);
+      }
+
+      if (values) {
+        const {data} = await axios.post('http://localhost:8000/auth/sign-up', values);
+        console.log(data);
+      }
+
+      
+    } catch (error) {
+      if (error instanceof axios.AxiosError) {
+        const response = error.response;
+        if (response){
+          console.log('Api Error: ', response.data.message);
+        }
+      }
+
+      console.log('Any error occured: ', (error as any).message);
+    }
+  };
+
+  const { email, name, password } = userInfo;
+
   return (
     <CustomKeyAvoidingView>
         <View style={styles.innerContainer}>
           <WelcomeHeader /> 
 
           <View style={styles.formContainer}>
-            <FormInput placeholder='Name' />
-            <FormInput placeholder='Email' keyboardType='email-address' autoCapitalize='none' />
-            <FormInput placeholder='Password' secureTextEntry={true}/>
+            <FormInput placeholder='Name' value={name} onChangeText={handleChange('name')}/>
+            <FormInput placeholder='Email' value={email} keyboardType='email-address' autoCapitalize='none' onChangeText={handleChange('email')} />
+            <FormInput placeholder='Password' value={password} secureTextEntry={true} onChangeText={handleChange('password')} />
 
-            <AppButton title='Sign Up' />
+            <AppButton active={false} title='Sign Up' onPress={handleSubmit}/>
 
             <FormDivider />
             <FormNavigator 
