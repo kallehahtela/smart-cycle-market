cf12e3feb983e82d31b4a7c5453bfd33e1389abf
Kalle Hahtela <khahtela@gmail.com>
2024-07-25 19:23:59 +0300

    Profile Page - Profile Info & Profile Page Options.

--- scm-app/app/utils/colors.ts
+++ scm-app/app/utils/colors.ts
@@ -1,8 +1,9 @@
 const primary = '#1c1c1f';
 const deActive = '#dce4f2';
+const active = '#0081f1';
 const white = '#fff';
 const backDrop = 'rgba(255, 255, 255, 0.5)';
 
-const colors = { primary, deActive, white, backDrop };
+const colors = { primary, deActive, active, white, backDrop };
 
 export default colors;
--- scm-app/app/views/Profile.tsx
+++ scm-app/app/views/Profile.tsx
@@ -1,19 +1,63 @@
 import { View, Text, StyleSheet, ScrollView } from 'react-native'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import AvatarView from './AvatarView';
+import useAuth from 'app/hooks/useAuth';
+import colors from '@utils/colors';
+import size from '@utils/size';
+import FormDivider from '@ui/FormDivider';
+import ProfileOptionListItem from '@components/ProfileOptionListItem';
 
 interface Props {}
 
 const Profile: FC<Props> = (props) => {
+  const { authState } = useAuth();
+  const { profile } = authState
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <AvatarView size={80}/>
+      <View style={styles.profileContainer}>
+        <AvatarView uri={authState.profile?.avatar} size={80} />
+
+
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>{profile?.name}</Text>
+          <Text style={styles.email}>{profile?.email}</Text>
+        </View>
+      </View>
+
+      <FormDivider />
+
+      <ProfileOptionListItem active style={styles.marginBottom} antIconName='message1' title='Messages' />
+      <ProfileOptionListItem style={styles.marginBottom} antIconName='appstore-o' title='Your Listings' />
+      <ProfileOptionListItem antIconName='logout' title='Log out' />
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    padding: size.padding,
+  },
+  profileContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  profileInfo: {
+    flex: 1,
+    paddingLeft: size.padding,
+  },
+  name: {
+    color: colors.primary,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  email: {
+    color: colors.primary,
+    paddingTop: 2,
+  },
+  marginBottom: {
+    marginBottom: 15,
+  }
 });
 
 export default Profile
